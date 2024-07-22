661a24c3e8b7d20d28d5019f6dd0312bb0eea4ac
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-07-22 18:11:00 +0530

    Written Navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { Box } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { CssBaseline } from "@mui/material"
-import { BrowserRouter, Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "@/scenes/navbar/Index"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
           <Routes>
             <Route path='/' element={<div>Dashboard Page</div>} />
             <Route path='/prediction' element={<div>Prediction Page</div>} />
