9005ac8996e1933ee56cd30924da942986688825
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-07-24 17:02:37 +0530

    setted up node typescript backend

---
+++ server/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(123);
+const addNumber = (a, b) => {
+    return a + b;
+};
+const sum = addNumber(1, 2);
+console.log(sum);
---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+console.log(123) ;
+
+const addNumber = (a:number , b:number)=>{
+    return a+b ;
+}
+
+const sum = addNumber(1,2) ;
+console.log(sum) ;
