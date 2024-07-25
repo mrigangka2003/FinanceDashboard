362b0ff512141e0e760d5129503296cd7eebeed5
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-07-25 11:52:08 +0530

    backend ts to js

--- server/build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log(123);
-const addNumber = (a, b) => {
-    return a + b;
-};
-const sum = addNumber(1, 2);
-console.log(sum);
--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-console.log(123) ;
-
-const addNumber = (a:number , b:number)=>{
-    return a+b ;
-}
-
-const sum = addNumber(1,2) ;
-console.log(sum) ;
